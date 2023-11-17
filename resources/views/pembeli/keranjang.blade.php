5375837d262ca066a01dd3f85316d1beff438728
andirchmd <andirchmd0@gmail.com>
2023-11-18 01:03:20 +0800

    ubah dikit pesanan+keranjang

--- resources/views/pembeli/keranjang.blade.php
+++ resources/views/pembeli/keranjang.blade.php
@@ -1,10 +1,13 @@
-<div class="fixed top-[10%] end-6 bg-[#3d68ff] h-[80%] mt-1 w-[34%] hidden sm:block shadow-xl rounded-xl">
+<div class="fixed top-[10%] end-6 bg-[#3d68ff] h-[80%] mt-1 w-[34%] hidden sm:block shadow-xl rounded-xl pb-2">
     <div class="px-4 h-auto">
-        <div class="">
-            <h1 class="text-lg font-medium ">Keranjang Anda</h1>
+        <div class="py-2">
+            <h1 class="text-lg font-medium">Keranjang Anda</h1>
         </div>
         <div class=" h-[75%] overflow-scroll overflow-x-hidden overflow-y-hidden">
             {{-- my path resources\views\pembeli\keranjang.blade.php --}}
+            @php
+                $grandTotal = 0; // Initialize the grand total variable
+            @endphp
             @foreach ($keranjangss as $index => $krj)
                 <div class="w-full flex flex-row mb-2">
                     <div class="  w-[30%] mr-2 h-[30%]">
@@ -13,14 +16,16 @@
                     <div class=" w-full flex flex-col">
                         {{-- <h1 class="font-medium text-lg">{{Nasi goreng tanpa di goreng}}</h1> --}}
                         <h1 class="font-medium text-lg">{{ $krj->nama }}</h1>
-                        <h1>Nasi goreng tanpa di goreng</h1>
+                        <h1>{{ $krj->deskripsi }}</h1>
 
                     </div>
                     <div class=" flex">
-                        <h1 class="fixed flex end-[5%] font-medium text-base">120000</h1>
+                        <h1 class="fixed flex end-[10%] font-medium text-base" id="totalPrice_{{ $index }}">
+                            {{ $krj->harga * $krj->jumlah }}
+                        </h1>
                         <div class="mt-2 flex flex-row px-2 items-center justify-center">
                             <button onclick="tambahNilai({{ $index }})">
-                                <div class=" text-black">
+                                <div class="text-black">
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                         stroke-width="1.5" stroke="currentColor" class="w-8 h-8">
                                         <path stroke-linecap="round" stroke-linejoin="round"
@@ -41,26 +46,41 @@
                                 </div>
                             </button>
                             <script>
-                                // var nilai = {{ $krj->jumlah }}; // Inisialisasi nilai awal
+                                document.addEventListener("DOMContentLoaded", function() {
+                                    var grandTotalElement = document.getElementById('grandTotalAmount');
+                                    var grandTotal = {{ $grandTotal }};
+                                    grandTotalElement.innerText = grandTotal;
+                                });
 
                                 function tambahNilai(index) {
-                                    // nilai += 1; // Menambah nilai
-                                    updateNilai(index, 1); // Memperbarui tampilan nilai
+                                    updateNilai(index, 1);
                                 }
 
                                 function kurangiNilai(index) {
-                                    // nilai -= 1; // Mengurangi nilai
-                                    updateNilai(index, -1); // Memperbarui tampilan nilai
+                                    updateNilai(index, -1);
                                 }
 
                                 function updateNilai(index, change) {
-                                    // Mengupdate tampilan nilai di dalam elemen dengan id "nilai"
                                     var element = document.querySelector('.nilai[indexx="' + index + '"]');
                                     var nilai = parseInt(element.innerText);
                                     nilai += change;
                                     element.innerText = nilai;
+
+                                    var harga = {{ $keranjangss[$index]->harga }};
+                                    var totalElement = document.getElementById('totalPrice_' + index);
+                                    var total = parseInt(totalElement.innerText);
+                                    total += harga * change;
+                                    totalElement.innerText = total;
+
+                                    var grandTotalElement = document.getElementById('grandTotalAmount');
+                                    var grandTotal = parseInt(grandTotalElement.innerText);
+                                    grandTotal += harga * change;
+                                    grandTotalElement.innerText = grandTotal;
                                 }
                             </script>
+                            @php
+                                $grandTotal += $krj->harga * $krj->jumlah;
+                            @endphp
                         </div>
                     </div>
                 </div>
@@ -70,7 +90,7 @@
     <div class="absolute bottom-0 w-full px-4 py-2 h-[18%] mt-auto bg-stone-300">
         <div class="flex flex-row py-1">
             <h1>Total</h1>
-            <h1 class="ml-auto text-2xl">18000</h1>
+            <h1 class="ml-auto text-2xl" id="grandTotalAmount">{{ $grandTotal }}</h1>
         </div>
         <a href="{{ route('pembeli.addPesanan') }}">
             <div class="bg-orange-600 w-full h-[55%] flex items-center justify-center">
