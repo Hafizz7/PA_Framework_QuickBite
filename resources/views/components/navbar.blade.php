1d03c6c60a4acfbdcd2e2a717007213ab2cb1f64
andirchmd <andirchmd0@gmail.com>
2023-11-17 13:41:32 +0800

    test account (fix navbar)

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,19 +1,24 @@
-<div class="flex flex-row p-2 justify-center  w-screen items-center bg-[#D9FFB0] sticky top-0 z-10">
-    <div class=" w-1/5 flex justify-center">
+<div class="flex flex-row p-2 justify-center w-screen items-center bg-[#D9FFB0] sticky top-0 z-10">
+    <div class="w-1/5 flex justify-center">
         <img src="{{ asset('assets/images/Logopng.png') }}" class="w-fit h-12">
     </div>
-    <div class=" w-screen text-center">
+    <div class="w-screen text-center">
         <ul class="flex flex-row gap-14 justify-center">
-            <li><a href="{{ route('penjual.addToko') }}">Home</a></li>
-            <li><a href="{{route('login')}}">Menu</a></li>
-            <li><a href="{{route('penjual.dashboard')}}">About</a></li>
+            <li class="hover:underline underline-offset-2"><a href="{{ route('penjual.addToko') }}">Home</a></li>
+            <li class="hover:underline underline-offset-2"><a href="{{ route('login') }}">Menu</a></li>
+            <li class="hover:underline underline-offset-2"><a href="{{ route('penjual.dashboard') }}">About</a></li>
         </ul>
     </div>
     <div class="w-1/5 flex text-center justify-center">
-        <a href="{{route('login')}}" class=" hover:text-xl"><div>Login</div></a>
+        @guest
+            <a href="{{ route('login') }}" class="hover:text-xl">
+                <div>Login</div>
+            </a>
+        @endguest
+        @auth
+            <a href="{{ route('logout') }}" class="hover:text-xl">
+                <div>Logout</div>
+            </a>
+        @endauth
     </div>
-    <div class="w-1/5 flex text-center justify-center">
-        <a href="{{route('logout')}}" class=" hover:text-xl"><div>Logout</div></a>
-    </div>
-
 </div>
